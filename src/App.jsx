b94ccad72efc9dc8a24e47c182c483952d2db658
Sahil <sahiltomar816@gmail.com>
2025-08-01 15:59:08 +0530

    "configure appwrite and redux store"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-green-400 text-3xl'>Tell World</h1>
+    </>
+  )
+}
+
+export default App
